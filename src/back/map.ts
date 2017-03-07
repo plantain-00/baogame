68cc320f1cd6df8f615dcfcc00297edf3088c038
York Yao <yaoao12306@outlook.com>
2017-03-07 12:57:08 +0800

    remove lessons

--- src/back/map.ts
+++ src/back/map.ts
@@ -11,137 +11,114 @@ export class Map {
     doors: common.Door[] = [];
     itemGates: services.ItemGate[] = [];
     signs: common.Sign[] = [];
-    constructor(public game: services.Game, data?: services.MapData) {
-        if (data) {
-            this.w = game.props.w;
-            this.h = game.props.h;
-            this.floor = data.floor;
-            this.ladders = data.ladders;
-            this.borns = data.borns;
-            this.onKilled = data.onKilled;
-            this.doors = data.doors;
-            this.itemGates = data.itemGates;
-            this.signs = data.signs;
-            if (data.npcs) {
-                for (const npcData of data.npcs) {
-                    const npc = this.game.createNPC({ name: npcData.name || "npc" });
-                    npc.x = (npcData.x + .5) * common.constant.tileWidth;
-                    npc.y = (npcData.y + .5) * common.constant.tileHeight;
-                    npc.carryCount = npcData.carryCount!;
-                    npc.carry = npcData.carry!;
-                    npc.AI = npcData.AI!;
-                }
-            }
-        } else {
-            // random map
-            this.w = game.props.tw;
-            this.h = game.props.th;
-            const w = this.w;
-            const h = this.h;
-            this.floor = [];
-            this.ladders = [];
+    constructor(public game: services.Game) {
+        this.w = game.props.tw;
+        this.h = game.props.th;
+        const w = this.w;
+        const h = this.h;
+        this.floor = [];
+        this.ladders = [];
 
-            for (let i = 0; i < h; i++) {
-                this.floor[i] = [];
-                let on = Math.random() > .5 ? 1 : 0;
-                if (i % 2 === 1 && i < h - 2) {
-                    for (let j = 0; j < w; j++) {
-                        if (i < 2 || i === h - 1 || j === 0 || j === w - 1) {
-                            this.floor[i][j] = 0;
-                        } else {
-                            this.floor[i][j] = on;
-                            if (Math.random() > .8) {
-                                on = 1 - on;
-                            }
+        for (let i = 0; i < h; i++) {
+            this.floor[i] = [];
+            let on = Math.random() > .5 ? 1 : 0;
+            if (i % 2 === 1 && i < h - 2) {
+                for (let j = 0; j < w; j++) {
+                    if (i < 2 || i === h - 1 || j === 0 || j === w - 1) {
+                        this.floor[i][j] = 0;
+                    } else {
+                        this.floor[i][j] = on;
+                        if (Math.random() > .8) {
+                            on = 1 - on;
                         }
                     }
                 }
             }
-
-            this.ladders.push({
-                x: 4.5,
-                y1: 1,
-                y2: h - 1,
-            });
-
-            this.ladders.push({
-                x: w - 3.5,
-                y1: 1,
-                y2: h - 1,
-            });
-
-            for (let j = 6; j < w - 6; j++) {
-                let start = 0;
-                let end = 0;
-                for (let i = 0; i < 4; i++) {
-                    if (this.floor[i][j]) {
-                        start = i;
-                        break;
-                    }
-                }
-                if (start) {
-                    for (let i = start + 1; i < h; i++) {
-                        if (this.floor[i][j]) {
-                            end = i;
-                        }
-                    }
-                    if (end) {
-                        this.ladders.push({
-                            x: j + .5,
-                            y1: start,
-                            y2: end + 1,
-                        });
-                    }
-                    j += 3;
-                }
-            }
-
-            this.floor[1][3] = 1;
-            this.floor[1][4] = 1;
-            this.floor[1][5] = 1;
-
-            this.floor[1][w - 3] = 1;
-            this.floor[1][w - 4] = 1;
-            this.floor[1][w - 5] = 1;
-
-            this.floor[3][2] = 1;
-            this.floor[3][3] = 1;
-            this.floor[3][4] = 1;
-            this.floor[3][5] = 1;
-
-            this.floor[3][w - 2] = 1;
-            this.floor[3][w - 3] = 1;
-            this.floor[3][w - 4] = 1;
-            this.floor[3][w - 5] = 1;
-
-            this.floor[h - 2][3] = 1;
-            this.floor[h - 2][4] = 1;
-            this.floor[h - 2][5] = 1;
-
-            this.floor[h - 2][w - 3] = 1;
-            this.floor[h - 2][w - 4] = 1;
-            this.floor[h - 2][w - 5] = 1;
-
-            this.borns = [];
-            for (let i = 0; i < 20; i++) {
-                const x = Math.floor(Math.random() * (this.w - 2)) + 1;
-                const y = Math.floor(Math.random() * (this.h - 2)) + 1;
-                if (this.floor[y][x]) {
-                    this.borns.push({ x, y });
-                }
-            }
-            for (const f of this.floor) {
-                for (let i = 0; i < this.w; i++) {
-                    if (f[i] !== 0 && f[i] !== 1) {
-                        f[i] = 0;
-                    }
-                }
-            }
-
-            this.itemGates.push({ x: 0, y: this.h / 2 });
-            this.itemGates.push({ x: this.w - 1, y: this.h / 2 });
-            this.itemGates.push({ x: this.w / 2, y: this.h - 1 });
         }
+
+        this.ladders.push({
+            x: 4.5,
+            y1: 1,
+            y2: h - 1,
+        });
+
+        this.ladders.push({
+            x: w - 3.5,
+            y1: 1,
+            y2: h - 1,
+        });
+
+        for (let j = 6; j < w - 6; j++) {
+            let start = 0;
+            let end = 0;
+            for (let i = 0; i < 4; i++) {
+                if (this.floor[i][j]) {
+                    start = i;
+                    break;
+                }
+            }
+            if (start) {
+                for (let i = start + 1; i < h; i++) {
+                    if (this.floor[i][j]) {
+                        end = i;
+                    }
+                }
+                if (end) {
+                    this.ladders.push({
+                        x: j + .5,
+                        y1: start,
+                        y2: end + 1,
+                    });
+                }
+                j += 3;
+            }
+        }
+
+        this.floor[1][3] = 1;
+        this.floor[1][4] = 1;
+        this.floor[1][5] = 1;
+
+        this.floor[1][w - 3] = 1;
+        this.floor[1][w - 4] = 1;
+        this.floor[1][w - 5] = 1;
+
+        this.floor[3][2] = 1;
+        this.floor[3][3] = 1;
+        this.floor[3][4] = 1;
+        this.floor[3][5] = 1;
+
+        this.floor[3][w - 2] = 1;
+        this.floor[3][w - 3] = 1;
+        this.floor[3][w - 4] = 1;
+        this.floor[3][w - 5] = 1;
+
+        this.floor[h - 2][3] = 1;
+        this.floor[h - 2][4] = 1;
+        this.floor[h - 2][5] = 1;
+
+        this.floor[h - 2][w - 3] = 1;
+        this.floor[h - 2][w - 4] = 1;
+        this.floor[h - 2][w - 5] = 1;
+
+        this.borns = [];
+        for (let i = 0; i < 20; i++) {
+            const x = Math.floor(Math.random() * (this.w - 2)) + 1;
+            const y = Math.floor(Math.random() * (this.h - 2)) + 1;
+            if (this.floor[y][x]) {
+                this.borns.push({ x, y });
+            }
+        }
+        for (const f of this.floor) {
+            for (let i = 0; i < this.w; i++) {
+                if (f[i] !== 0 && f[i] !== 1) {
+                    f[i] = 0;
+                }
+            }
+        }
+
+        this.itemGates.push({ x: 0, y: this.h / 2 });
+        this.itemGates.push({ x: this.w - 1, y: this.h / 2 });
+        this.itemGates.push({ x: this.w / 2, y: this.h - 1 });
     }
     born() {
         const i = Math.floor(Math.random() * this.borns.length);
