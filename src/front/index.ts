5484a95479b6e81b171bc77b8747e5871bec8f6c
york yao <yaoao12306@outlook.com>
2017-03-15 21:32:32 +0800

    merge controllers

--- src/front/index.ts
+++ src/front/index.ts
@@ -1,6 +1,4 @@
 import { connect, emit } from "./socket";
-import * as pcController from "./pcController";
-import * as mobileController from "./pcController";
 import * as common from "../back/common";
 import * as images from "./images";
 import * as flare from "./effects/flare";
@@ -12,6 +10,10 @@ import * as libs from "./libs";
 
 /* tslint:disable:no-unused-new */
 
+const isMobile = navigator.userAgent.indexOf("iPhone") > -1
+    || navigator.userAgent.indexOf("Android") > -1
+    || navigator.userAgent.indexOf("iPad") > -1;
+
 const app: any = new libs.Vue({
     el: "#container",
     data: {
@@ -19,7 +21,7 @@ const app: any = new libs.Vue({
         userName: localStorage.getItem("userName") || "无名小卒",
         showDialog: false,
         showFail: false,
-        showMobileControl: false,
+        showMobileControl: isMobile,
     },
     methods: {
         join(this: libs.App) {
@@ -87,11 +89,6 @@ const app: any = new libs.Vue({
     },
 });
 
-const control = (navigator.userAgent.indexOf("iPhone") === -1
-    && navigator.userAgent.indexOf("Android") === -1
-    && navigator.userAgent.indexOf("iPad") === -1)
-    ? pcController.start(app, initDone)
-    : mobileController.start(app, initDone);
 let currentUserId: number;
 let currentUser: common.User;
 
@@ -128,96 +125,6 @@ const game: {
     };
 
 let lastControl: string;
-
-function initDone() {
-    connect(protocol => {
-        if (protocol.kind === "initSuccess") {
-            const cdom = document.createElement("canvas");	// 玩家层
-            const cdomBg = document.createElement("canvas");	// 背景层（只绘制一次）
-            cdomBody = document.createElement("canvas");	// 标记层（只添加，不修改，尸体）
-            cdom.width = common.w;
-            cdom.height = common.h;
-            cdom.id = "fg";
-            cdomBg.width = common.w;
-            cdomBg.height = common.h;
-            cdomBg.id = "bg";
-            cdomBody.width = common.w;
-            cdomBody.height = common.h;
-            context = cdom.getContext("2d")!;
-            context.font = "14px 宋体";
-            context.textBaseline = "middle"; // 设置文本的垂直对齐方式
-            context.textAlign = "center"; // 设置文本的水平对对齐方式
-
-            ctxBg = cdomBg.getContext("2d");
-            ctxBody = cdomBody.getContext("2d")!;
-            ctxBody.font = "14px 宋体";
-            ctxBody.textBaseline = "middle"; // 设置文本的垂直对齐方式
-            ctxBody.textAlign = "center"; // 设置文本的水平对对齐方式
-
-            drawer.drawBg(ctxBg!, protocol.initSuccess.map, common.w, common.h);
-            game.itemGates = protocol.initSuccess.map.itemGates;
-            game.doors = protocol.initSuccess.map.doors;
-            game.signs = protocol.initSuccess.map.signs;
-            const middle = document.getElementById("middle")!;
-            middle.innerHTML = "";
-            middle.appendChild(cdomBg);
-            middle.appendChild(cdom);
-            app.showDialog = true;
-        } else if (protocol.kind === "joinSuccess") {
-            currentUserId = protocol.userId;
-            app.showDialog = false;
-        } else if (protocol.kind === "tick") {
-            t++;
-            for (let i = 0; i < protocol.tick.users.length; i++) {
-                if (protocol.tick.users[i].id === currentUserId) {
-                    currentUser = protocol.tick.users[i];
-                    break;
-                }
-            }
-
-            render(context, protocol);
-
-            const controlProtocol: common.ControlProtocol = {
-                kind: "control",
-                control,
-            };
-            const thisControl = JSON.stringify(controlProtocol);
-            if (thisControl !== lastControl) {
-                lastControl = thisControl;
-                emit(controlProtocol);
-            }
-            let userCount = 0;
-            for (const user of protocol.tick.users) {
-                if (!user.npc) {
-                    userCount++;
-                }
-            }
-            control.leftPress = false;
-            control.rightPress = false;
-            control.upPress = false;
-            control.downPress = false;
-            control.itemPress = false;
-        } else if (protocol.kind === "explode") {
-            cdx = 8;
-            cdy = 9;
-            drawer.flares.push(flare.create(protocol.explode.x, protocol.explode.y, protocol.explode.power, common.h, true));
-        } else if (protocol.kind === "userDead") {
-            app.notices.push(protocol.userDead.message);
-            if (protocol.userDead.user.id === currentUserId) {
-                setTimeout(() => {
-                    app.showFail = true;
-                    app.showDialog = true;
-                }, 500);
-            }
-            if (protocol.userDead.killer) {
-                const killer = protocol.userDead.killer;
-                if (killer.score <= 10) {
-                    drawer.toasts.push(toast.create(killer.x, killer.y, killer.score * 1.5 + 14, killer.name + scoreText[killer.score - 1], common.h));
-                }
-            }
-        }
-    });
-}
 
 function render(ctx: CanvasRenderingContext2D, protocol: common.TickProtocol) {
     ctx.clearRect(0, 0, common.w, common.h);
@@ -282,3 +189,170 @@ function render(ctx: CanvasRenderingContext2D, protocol: common.TickProtocol) {
 
     ctx.restore();
 }
+
+const control: common.Control = {
+    upDown: 0,
+    downDown: 0,
+    leftDown: 0,
+    rightDown: 0,
+    itemDown: 0,
+    itemPress: false,
+    leftPress: false,
+    rightPress: false,
+    upPress: false,
+    downPress: false,
+};
+
+document.addEventListener("keydown", e => {
+    if (e.keyCode === 87) {
+        if (!control.upDown) {
+            control.upPress = true;
+        }
+        control.upDown = 2000;
+    } else if (e.keyCode === 83) {
+        if (!control.downDown) {
+            control.downPress = true;
+        }
+        control.downDown = 2000;
+    } else if (e.keyCode === 65) {
+        if (!control.leftDown) {
+            control.leftPress = true;
+        }
+        control.leftDown = 2000;
+    } else if (e.keyCode === 68) {
+        if (!control.rightDown) {
+            control.rightPress = true;
+        }
+        control.rightDown = 2000;
+    } else if (e.keyCode === 81) {
+        if (!control.itemDown) {
+            control.itemPress = true;
+        }
+        control.itemDown = 2000;
+    }
+});
+document.addEventListener("keyup", e => {
+    if (e.keyCode === 87) {
+        control.upDown = 0;
+    } else if (e.keyCode === 83) {
+        control.downDown = 0;
+    } else if (e.keyCode === 65) {
+        control.leftDown = 0;
+    } else if (e.keyCode === 68) {
+        control.rightDown = 0;
+    } else if (e.keyCode === 81) {
+        control.itemDown = 0;
+    }
+    if (e.keyCode === 69) {
+        if (app.showDialog) {
+            app.join();
+        }
+    }
+    e.preventDefault();
+});
+
+const middle = document.getElementById("middle")!;
+function checkDisplay() {
+    if (600 > window.innerHeight) {
+        const h = window.innerHeight;
+        const w = Math.floor(h / 600 * 1100);
+        middle.style.width = `${w}px`;
+        middle.style.height = `${h}px`;
+    } else {
+        middle.style.removeProperty("width");
+        middle.style.removeProperty("height");
+    }
+}
+
+if (isMobile) {
+    document.ontouchmove = e => e.preventDefault;
+    checkDisplay();
+    window.onresize = checkDisplay;
+}
+
+connect(protocol => {
+    if (protocol.kind === "initSuccess") {
+        const cdom = document.createElement("canvas");	// 玩家层
+        const cdomBg = document.createElement("canvas");	// 背景层（只绘制一次）
+        cdomBody = document.createElement("canvas");	// 标记层（只添加，不修改，尸体）
+        cdom.width = common.w;
+        cdom.height = common.h;
+        cdom.id = "fg";
+        cdomBg.width = common.w;
+        cdomBg.height = common.h;
+        cdomBg.id = "bg";
+        cdomBody.width = common.w;
+        cdomBody.height = common.h;
+        context = cdom.getContext("2d")!;
+        context.font = "14px 宋体";
+        context.textBaseline = "middle"; // 设置文本的垂直对齐方式
+        context.textAlign = "center"; // 设置文本的水平对对齐方式
+
+        ctxBg = cdomBg.getContext("2d");
+        ctxBody = cdomBody.getContext("2d")!;
+        ctxBody.font = "14px 宋体";
+        ctxBody.textBaseline = "middle"; // 设置文本的垂直对齐方式
+        ctxBody.textAlign = "center"; // 设置文本的水平对对齐方式
+
+        drawer.drawBg(ctxBg!, protocol.initSuccess.map, common.w, common.h);
+        game.itemGates = protocol.initSuccess.map.itemGates;
+        game.doors = protocol.initSuccess.map.doors;
+        game.signs = protocol.initSuccess.map.signs;
+        middle.innerHTML = "";
+        middle.appendChild(cdomBg);
+        middle.appendChild(cdom);
+        app.showDialog = true;
+    } else if (protocol.kind === "joinSuccess") {
+        currentUserId = protocol.userId;
+        app.showDialog = false;
+    } else if (protocol.kind === "tick") {
+        t++;
+        for (let i = 0; i < protocol.tick.users.length; i++) {
+            if (protocol.tick.users[i].id === currentUserId) {
+                currentUser = protocol.tick.users[i];
+                break;
+            }
+        }
+
+        render(context, protocol);
+
+        const controlProtocol: common.ControlProtocol = {
+            kind: "control",
+            control,
+        };
+        const thisControl = JSON.stringify(controlProtocol);
+        if (thisControl !== lastControl) {
+            lastControl = thisControl;
+            emit(controlProtocol);
+        }
+        let userCount = 0;
+        for (const user of protocol.tick.users) {
+            if (!user.npc) {
+                userCount++;
+            }
+        }
+        control.leftPress = false;
+        control.rightPress = false;
+        control.upPress = false;
+        control.downPress = false;
+        control.itemPress = false;
+    } else if (protocol.kind === "explode") {
+        cdx = 8;
+        cdy = 9;
+        drawer.flares.push(flare.create(protocol.explode.x, protocol.explode.y, protocol.explode.power, common.h, true));
+    } else if (protocol.kind === "userDead") {
+        app.notices.push(protocol.userDead.message);
+        if (protocol.userDead.user.id === currentUserId) {
+            setTimeout(() => {
+                app.showFail = true;
+                app.showDialog = true;
+            }, 500);
+        }
+        if (protocol.userDead.killer) {
+            const killer = protocol.userDead.killer;
+            if (killer.score <= 10) {
+                drawer.toasts.push(toast.create(killer.x, killer.y, killer.score * 1.5 + 14, killer.name + scoreText[killer.score - 1], common.h));
+            }
+        }
+    }
+});
