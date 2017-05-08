c174bec9b5fc126048209de3c38699882f789dd1
york yao <yaoao12306@outlook.com>
2017-05-08 21:32:16 +0800

    give images version

--- src/front/images.ts
+++ src/front/images.ts
@@ -1,63 +1,65 @@
+import * as variables from "./variables";
+
 export const happy = new Image();
-happy.src = "./imgs/head/happy.png";
+happy.src = `./imgs/head/${variables.staticImgsHeadHappyPng}`;
 
 export const throll = new Image();
-throll.src = "./imgs/head/throll.png";
+throll.src = `./imgs/head/${variables.staticImgsHeadThrollPng}`;
 
 export const danger = new Image();
-danger.src = "./imgs/head/danger3.png";
+danger.src = `./imgs/head/${variables.staticImgsHeadDanger3Png}`;
 
 export const alone = new Image();
-alone.src = "./imgs/head/alone.png";
+alone.src = `./imgs/head/${variables.staticImgsHeadAlonePng}`;
 
 export const normal = new Image();
-normal.src = "./imgs/head/normal.png";
+normal.src = `./imgs/head/${variables.staticImgsHeadNormalPng}`;
 
 export const win = new Image();
-win.src = "./imgs/head/win.png";
+win.src = `./imgs/head/${variables.staticImgsHeadWinPng}`;
 
 export const wtf = new Image();
-wtf.src = "./imgs/head/wtf.png";
+wtf.src = `./imgs/head/${variables.staticImgsHeadWtfPng}`;
 
 export const sign = new Image();
-sign.src = "./imgs/tile/sign.png";
+sign.src = `./imgs/tile/${variables.staticImgsTileSignPng}`;
 
 export const door = new Image();
-door.src = "./imgs/tile/door.png";
+door.src = `./imgs/tile/${variables.staticImgsTileDoorPng}`;
 
 export const itemGate = new Image();
-itemGate.src = "./imgs/tile/itemGate.png";
+itemGate.src = `./imgs/tile/${variables.staticImgsTileItemGatePng}`;
 
 export const bomb = new Image();
-bomb.src = "./imgs/bomb.png";
+bomb.src = `./imgs/${variables.staticImgsBombPng}`;
 
 export const grenade = new Image();
-grenade.src = "./imgs/grenade.png";
+grenade.src = `./imgs/${variables.staticImgsGrenadePng}`;
 
 export const arm = new Image();
-arm.src = "./imgs/arm.png";
+arm.src = `./imgs/${variables.staticImgsArmPng}`;
 
 export const mine = new Image();
-mine.src = "./imgs/mine.png";
+mine.src = `./imgs/${variables.staticImgsMinePng}`;
 
 export const jet = new Image();
-jet.src = "./imgs/jet.png";
+jet.src = `./imgs/${variables.staticImgsJetPng}`;
 
 const itemPower = new Image();
-itemPower.src = "./imgs/item/power.png";
+itemPower.src = `./imgs/item/${variables.staticImgsItemPowerPng}`;
 const itemGun = new Image();
-itemGun.src = "./imgs/item/gun.png";
+itemGun.src = `./imgs/item/${variables.staticImgsItemGunPng}`;
 const itemMine = new Image();
-itemMine.src = "./imgs/item/mine.png";
+itemMine.src = `./imgs/item/${variables.staticImgsItemMinePng}`;
 const itemDrug = new Image();
-itemDrug.src = "./imgs/item/drug.png";
+itemDrug.src = `./imgs/item/${variables.staticImgsItemDrugPng}`;
 const itemHide = new Image();
-itemHide.src = "./imgs/item/hide.png";
+itemHide.src = `./imgs/item/${variables.staticImgsItemHidePng}`;
 const itemRandom = new Image();
-itemRandom.src = "./imgs/item/random.png";
+itemRandom.src = `./imgs/item/${variables.staticImgsItemRandomPng}`;
 const itemFlypack = new Image();
-itemFlypack.src = "./imgs/item/flypack.png";
+itemFlypack.src = `./imgs/item/${variables.staticImgsItemFlypackPng}`;
 const itemGrenade = new Image();
-itemGrenade.src = "./imgs/item/grenade.png";
+itemGrenade.src = `./imgs/item/${variables.staticImgsItemGrenadePng}`;
 
 export const items: HTMLImageElement[] = [itemPower, itemGun, itemMine, itemDrug, itemHide, itemRandom, itemRandom, itemFlypack, itemGrenade];
--- static/rev-static.config.js
+++ static/rev-static.config.js
@@ -3,6 +3,10 @@ module.exports = {
         "static/scripts/index.js",
         "static/index.css",
         "static/*.ejs.html",
+        "static/imgs/**/*.png",
+    ],
+    excludeFiles: [
+        "static/imgs/**/*-*.png",
     ],
     outputFiles: file => file.replace(".ejs", ""),
     json: false,
@@ -12,4 +16,5 @@ module.exports = {
     sha: 256,
     customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
     noOutputFiles: [],
+    es6: "src/front/variables.ts",
 };
