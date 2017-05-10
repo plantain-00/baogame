7ced181ce52fe1626ac175d63db251aa1b05af7b
york yao <yaoao12306@outlook.com>
2017-05-10 20:33:35 +0800

    split rev-static config file

--- static/rev-static.image.js
+++ static/rev-static.image.js
@@ -1,10 +1,10 @@
 module.exports = {
     inputFiles: [
-        "static/scripts/index.js",
-        "static/index.css",
-        "static/*.ejs.html",
+        "static/imgs/**/*.png",
     ],
-    excludeFiles: [],
+    excludeFiles: [
+        "static/imgs/**/*-*.png",
+    ],
     outputFiles: file => file.replace(".ejs", ""),
     json: false,
     ejsOptions: {
@@ -13,4 +13,5 @@ module.exports = {
     sha: 256,
     customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
     noOutputFiles: [],
+    es6: "src/front/variables.ts",
 };
