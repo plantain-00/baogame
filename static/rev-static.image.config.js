5a7d4fff12e24596ba073385c3cc00061b5eea72
york yao <yaoao12306@outlook.com>
2017-06-30 21:50:34 +0800

    lint config js file

--- static/rev-static.image.config.js
+++ static/rev-static.image.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   inputFiles: [
-    'static/scripts/index.js',
-    'static/index.css',
-    'static/*.ejs.html'
+    'static/imgs/**/*.png'
   ],
-  excludeFiles: [],
+  excludeFiles: [
+    'static/imgs/**/*-*.png'
+  ],
   outputFiles: file => file.replace('.ejs', ''),
   json: false,
   ejsOptions: {
@@ -12,5 +12,6 @@ module.exports = {
   },
   sha: 256,
   customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
-  noOutputFiles: []
+  noOutputFiles: [],
+  es6: 'src/front/variables.ts'
 }
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,33 +1,33 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
-    entry: {
-        index: "./static/dist/front/index",
-    },
-    output: {
-        path: path.join(__dirname, "scripts/"),
-        filename: "[name].js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.min.js"
-        }
+  entry: {
+    index: './static/dist/front/index'
+  },
+  output: {
+    path: path.join(__dirname, 'scripts/'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.min.js'
     }
-};
+  }
+}
