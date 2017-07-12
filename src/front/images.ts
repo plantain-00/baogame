dbcb1f6ce7951a3c95ef7ba9c6efd39e72705949
york yao <yaoao12306@outlook.com>
2017-07-12 19:16:19 +0800

    bundle images into js files

--- src/front/images.ts
+++ src/front/images.ts
@@ -1,65 +1,65 @@
 import * as variables from "./variables";
 
 export const happy = new Image();
-happy.src = `./imgs/head/${variables.headHappyPng}`;
+happy.src = variables.headHappyPng;
 
 export const throll = new Image();
-throll.src = `./imgs/head/${variables.headThrollPng}`;
+throll.src = variables.headThrollPng;
 
 export const danger = new Image();
-danger.src = `./imgs/head/${variables.headDanger3Png}`;
+danger.src = variables.headDanger3Png;
 
 export const alone = new Image();
-alone.src = `./imgs/head/${variables.headAlonePng}`;
+alone.src = variables.headAlonePng;
 
 export const normal = new Image();
-normal.src = `./imgs/head/${variables.headNormalPng}`;
+normal.src = variables.headNormalPng;
 
 export const win = new Image();
-win.src = `./imgs/head/${variables.headWinPng}`;
+win.src = variables.headWinPng;
 
 export const wtf = new Image();
-wtf.src = `./imgs/head/${variables.headWtfPng}`;
+wtf.src = variables.headWtfPng;
 
 export const sign = new Image();
-sign.src = `./imgs/tile/${variables.tileSignPng}`;
+sign.src = variables.tileSignPng;
 
 export const door = new Image();
-door.src = `./imgs/tile/${variables.tileDoorPng}`;
+door.src = variables.tileDoorPng;
 
 export const itemGate = new Image();
-itemGate.src = `./imgs/tile/${variables.tileItemGatePng}`;
+itemGate.src = variables.tileItemGatePng;
 
 export const bomb = new Image();
-bomb.src = `./imgs/${variables.bombPng}`;
+bomb.src = variables.bombPng;
 
 export const grenade = new Image();
-grenade.src = `./imgs/${variables.grenadePng}`;
+grenade.src = variables.grenadePng;
 
 export const arm = new Image();
-arm.src = `./imgs/${variables.armPng}`;
+arm.src = variables.armPng;
 
 export const mine = new Image();
-mine.src = `./imgs/${variables.minePng}`;
+mine.src = variables.minePng;
 
 export const jet = new Image();
-jet.src = `./imgs/${variables.jetPng}`;
+jet.src = variables.jetPng;
 
 const itemPower = new Image();
-itemPower.src = `./imgs/item/${variables.itemPowerPng}`;
+itemPower.src = variables.itemPowerPng;
 const itemGun = new Image();
-itemGun.src = `./imgs/item/${variables.itemGunPng}`;
+itemGun.src = variables.itemGunPng;
 const itemMine = new Image();
-itemMine.src = `./imgs/item/${variables.itemMinePng}`;
+itemMine.src = variables.itemMinePng;
 const itemDrug = new Image();
-itemDrug.src = `./imgs/item/${variables.itemDrugPng}`;
+itemDrug.src = variables.itemDrugPng;
 const itemHide = new Image();
-itemHide.src = `./imgs/item/${variables.itemHidePng}`;
+itemHide.src = variables.itemHidePng;
 const itemRandom = new Image();
-itemRandom.src = `./imgs/item/${variables.itemRandomPng}`;
+itemRandom.src = variables.itemRandomPng;
 const itemFlypack = new Image();
-itemFlypack.src = `./imgs/item/${variables.itemFlypackPng}`;
+itemFlypack.src = variables.itemFlypackPng;
 const itemGrenade = new Image();
-itemGrenade.src = `./imgs/item/${variables.itemGrenadePng}`;
+itemGrenade.src = variables.itemGrenadePng;
 
 export const items: HTMLImageElement[] = [itemPower, itemGun, itemMine, itemDrug, itemHide, itemRandom, itemRandom, itemFlypack, itemGrenade];
