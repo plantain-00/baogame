1f81f4d689ef19edf2f069daeca6d59aa505b338
york yao <yaoao12306@outlook.com>
2017-07-14 07:25:06 +0800

    add release script

---
+++ clean-release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  include: [
+    'dist/*.js',
+    'static/scripts/*.bundle-*.js',
+    'static/index.html',
+    'LICENSE',
+    'package.json',
+    'README.md'
+  ],
+  exclude: [
+  ],
+  releaseRepository: 'https://github.com/plantain-00/baogame-release.git'
+}
