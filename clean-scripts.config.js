e35b405374f8b8c03a2c1a8689416baf8e84267e
york yao <yaoao12306@outlook.com>
2017-10-27 09:47:06 +0800

    update dependencies

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,8 +1,4 @@
-const childProcess = require('child_process')
-const util = require('util')
-const { Service } = require('clean-scripts')
-
-const execAsync = util.promisify(childProcess.exec)
+const { Service, execAsync } = require('clean-scripts')
 
 const tsFiles = `"src/**/*.ts" "spec/**/*.ts" "static_spec/**/*.ts" "screenshots/**/*.ts"`
 const jsFiles = `"*.config.js" "static/**/*.config.js" "static_spec/**/*.config.js"`
