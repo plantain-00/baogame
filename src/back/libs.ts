9f2e39746616e2f5ddfd0eb3c119e0aa3cbfb048
york yao <yaoao12306@outlook.com>
2018-02-10 22:54:52 +0800

    refactor: clean tsconfig.json

--- src/back/libs.ts
+++ src/back/libs.ts
@@ -1,9 +1,9 @@
 import * as url from 'url'
-import * as express from 'express'
+import express from 'express'
 import * as http from 'http'
 import * as path from 'path'
-import * as WebSocket from 'uws'
-import * as minimist from 'minimist'
+import WebSocket from 'uws'
+import minimist from 'minimist'
 import * as protobuf from 'protobufjs'
 
 export const WebSocketServer = WebSocket.Server
