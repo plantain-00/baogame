5f4de389d497b816d1fa93e4b661f1769e66db6d
york yao <yaoao12306@outlook.com>
2018-09-02 09:10:40 +0800

    chore: simplify package

--- src/front/libs.ts
+++ src/front/libs.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import Reconnector from 'reconnection/browser/browser'
+import Reconnector from 'reconnection'
 
 export { Vue, Reconnector, Component }
